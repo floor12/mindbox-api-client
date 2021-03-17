a4ac217844e10b98dfd82c4740dd11ca3abf5e45
floor12 <floor12@floor12.net>
2021-03-17 15:41:32 +0100

    Refactoring.

--- src/MindBoxRequest.php
+++ src/MindBoxRequest.php
@@ -6,90 +6,56 @@ namespace floor12\MindBox;
 
 class MindBoxRequest
 {
-    /** @var string */
-    protected $operationName;
-    /** @var array */
-    protected $body = [];
-    /** @var string|null */
+    protected string $operationName;
+    protected array|null $body = null;
     protected $deviceUUID;
-    /** @var int */
     protected $mode = MindBoxClient::MODE_SYNCHRONOUS;
 
-    /**
-     * @return string
-     */
-    public function getOperationName()
+    public function getOperationName(): string
     {
         return $this->operationName;
     }
 
-    /**
-     * @return string|null
-     */
     public function getDeviceUUID(): ?string
     {
         return $this->deviceUUID;
     }
 
-    /**
-     * @return array
-     */
-    public function getBody(): array
+    public function getBody(): ?array
     {
         return $this->body;
     }
 
-    /**
-     * @return string
-     */
     public function getBodyAsJson(): string
     {
         return json_encode($this->body);
     }
 
-    /**
-     * @return int
-     */
     public function getMode(): int
     {
         return $this->mode;
     }
 
-    /**
-     * @return bool
-     */
     public function isAsync(): bool
     {
         return $this->mode === MindBoxClient::MODE_ASYNCHRONOUS;
     }
 
-    /**
-     * @param string|null $deviceUUID
-     */
     public function setDeviceUUID(?string $deviceUUID): void
     {
         $this->deviceUUID = $deviceUUID;
     }
 
-    /**
-     * @param int $mode
-     */
     public function setMode(int $mode): void
     {
         $this->mode = $mode;
     }
 
-    /**
-     * @param mixed $operationName
-     */
     public function setOperationName($operationName): void
     {
         $this->operationName = $operationName;
     }
 
-    /**
-     * @param array $body
-     */
     public function setBody(array $body): void
     {
         $this->body = $body;
